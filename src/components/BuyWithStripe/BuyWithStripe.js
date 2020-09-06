6ae126c64837c5df34b663a87ab0b98aa28b993f
colbyfayock <fayock@gmail.com>
2020-09-06 13:49:09 -0400

    butotn

--- src/components/BuyWithStripe/BuyWithStripe.js
+++ src/components/BuyWithStripe/BuyWithStripe.js
@@ -5,7 +5,7 @@ import LogoStripe from '../LogoStripe';
 
 const BuyWithStripe = ({ children, prefix = 'Buy with', ...rest }) => {
   return (
-    <Button className={styles.button} role="link">
+    <Button className={styles.button} role="link" {...rest}>
       { prefix } <LogoStripe />
     </Button>
   )
