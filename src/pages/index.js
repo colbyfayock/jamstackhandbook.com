a47be65ef1b3f0fb42347530cbf20a9275b4e4af
colbyfayock <fayock@gmail.com>
2020-09-06 13:31:31 -0400

    simplifying code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,48 +1,48 @@
 import { useReducer } from 'react';
 import Head from 'next/head'
-import { loadStripe } from '@stripe/stripe-js';
 
 import styles from '../styles/Home.module.scss'
 
 import Footer from '../components/Footer';
 
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
+import { initCheckout, formatPrice } from '../lib/payments';
+
+const PRODUCT_PRICE = process.env.NEXT_PUBLIC_BASE_PRICE;
+const PRODUCT_CURRENCY = process.env.NEXT_PUBLIC_CURRENCY;
 
 export default function Home() {
 
   const [state, dispatch] = useReducer(reducer, {
-    priceId: process.env.NEXT_PUBLIC_PRICE_ID,
-    basePrice: process.env.NEXT_PUBLIC_BASE_PRICE,
-    currency: process.env.NEXT_PUBLIC_CURRENCY,
-    quantity: 1,
-    price: formatPrice({
-      amount: process.env.NEXT_PUBLIC_BASE_PRICE,
-      currency: process.env.NEXT_PUBLIC_CURRENCY,
-      quantity: 1,
-    }),
     loading: false,
     error: null,
   });
 
-  const handleClick = async (event) => {
-    // Call your backend to create the Checkout session.
-    dispatch({ type: 'setLoading', payload: { loading: true } });
-    // When the customer clicks on the button, redirect them to Checkout.
-    const stripe = await stripePromise;
-    const { error } = await stripe.redirectToCheckout({
-      mode: 'payment',
-      lineItems: [{ price: state.priceId, quantity: state.quantity }],
-      successUrl: `${window.location.origin}/success?session_id={CHECKOUT_SESSION_ID}`,
-      cancelUrl: `${window.location.origin}/canceled`,
-    });
-    // If `redirectToCheckout` fails due to a browser or network
-    // error, display the localized error message to your customer
-    // using `error.message`.
-    if (error) {
-      dispatch({ type: 'setError', payload: { error } });
-      dispatch({ type: 'setLoading', payload: { loading: false } });
+  const cost = formatPrice({
+    amount: PRODUCT_PRICE,
+    currency: PRODUCT_CURRENCY,
+    quantity: 1
+  });
+
+  /**
+   * handleOnPurchase
+   */
+
+  async function handleOnPurchase() {
+    dispatch(setLoading(true));
+    try {
+      await initCheckout({
+        lineItems: [
+          {
+            price: process.env.NEXT_PUBLIC_PRICE_ID,
+            quantity: 1
+          }
+        ]
+      });
+    } catch(e) {
+      dispatch(setLoading(false));
+      dispatch(setError(e));
     }
-  };
+  }
 
   return (
     <div className={styles.container}>
@@ -61,61 +61,21 @@ export default function Home() {
         </p>
 
         <p className={styles.description}>
-          Get updates to your inbox!
+          Learn everything you need to know about the Jamstack including
+          3 tutorials to learn by doing for only { cost }
         </p>
 
-        <form className={styles.form} action="https://app.convertkit.com/forms/1646524/subscriptions" method="post">
-          <label className={styles.sronly} htmlFor="email">Email Address</label>
-          <input type="email" name="email_address" placeholder="Email Address" required />
-          <button>Sign Up</button>
-        </form>
+        <p>
+          Want to bulk order? Reach out at hello@colbyfayock.com
+        </p>
 
-        <section className="container">
-          <div>
-            <h1>Single photo</h1>
-            <h4>Purchase a Pasha original photo</h4>
-            <div className="pasha-image">
-              <img
-                alt="Random asset from Picsum"
-                src="https://picsum.photos/280/320?random=4"
-                width="140"
-                height="160"
-              />
-            </div>
-          </div>
-          <div className="quantity-setter">
-            <button
-              className="increment-btn"
-              disabled={state.quantity === 1}
-              onClick={() => dispatch({ type: 'decrement' })}
-            >
-              -
-            </button>
-            <input
-              type="number"
-              id="quantity-input"
-              min="1"
-              max="10"
-              value={state.quantity}
-              readOnly
-            />
-            <button
-              className="increment-btn"
-              disabled={state.quantity === 10}
-              onClick={() => dispatch({ type: 'increment' })}
-            >
-              +
-            </button>
-          </div>
-          <p className="sr-legal-text">Number of copies (max 10)</p>
+        <button role="link" onClick={handleOnPurchase} disabled={state.loading}>
+          {state.loading ? `Loading...` : `Buy Now`}
+        </button>
 
-          <button role="link" onClick={handleClick} disabled={state.loading}>
-            {state.loading || !state.price
-              ? `Loading...`
-              : `Buy for ${state.price}`}
-          </button>
-          <div className="sr-field-error">{state.error?.message}</div>
-        </section>
+        <div>
+          {state.error?.message}
+        </div>
 
       </main>
 
@@ -125,48 +85,26 @@ export default function Home() {
   )
 }
 
-
-
-function formatPrice({ amount, currency, quantity }) {
-  const numberFormat = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency,
-    currencyDisplay: 'symbol',
-  });
-  const parts = numberFormat.formatToParts(amount);
-  let zeroDecimalCurrency = true;
-  for (let part of parts) {
-    if (part.type === 'decimal') {
-      zeroDecimalCurrency = false;
+function setLoading(isLoading) {
+  return {
+    type: 'setLoading',
+    payload: {
+      loading: isLoading
     }
   }
-  amount = zeroDecimalCurrency ? amount : amount / 100;
-  const total = (quantity * amount).toFixed(2);
-  return numberFormat.format(total);
-};
+}
+
+function setError(error) {
+  return {
+    type: 'setError',
+    payload: {
+      error
+    }
+  }
+}
 
 function reducer(state, action) {
   switch (action.type) {
-    case 'increment':
-      return {
-        ...state,
-        quantity: state.quantity + 1,
-        price: formatPrice({
-          amount: state.basePrice,
-          currency: state.currency,
-          quantity: state.quantity + 1,
-        }),
-      };
-    case 'decrement':
-      return {
-        ...state,
-        quantity: state.quantity - 1,
-        price: formatPrice({
-          amount: state.basePrice,
-          currency: state.currency,
-          quantity: state.quantity - 1,
-        }),
-      };
     case 'setLoading':
       return { ...state, loading: action.payload.loading };
     case 'setError':
