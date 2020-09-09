be076423f8a6d56a4831fe38e812b5a8de8ef351
colbyfayock <fayock@gmail.com>
2020-09-09 15:40:11 -0400

    preorder share

--- src/pages/success.js
+++ src/pages/success.js
@@ -2,13 +2,33 @@ import Head from 'next/head'
 
 import styles from '../styles/Home.module.scss'
 
+import { createTweetAction, openTweet } from '../lib/social';
+
 import Main from '../components/Main';
 import Section from '../components/Section';
 import Hero from '../components/Hero';
 import Container from '../components/Container';
 import Footer from '../components/Footer';
+import Button from '../components/Button';
 
-export default function Home() {
+export default function Success() {
+  const twitterAction = createTweetAction({
+    message: [
+      '💥 I just pre-ordered Jamstack Handbook from @colbyfayock\n',
+      '⚡️ It shows you how to build fast dynamic #webdev apps w/ #javascript and the static web\n',
+      '👉 Go grab your copy! Theres a preorder special with a FREE sticker pack\n',
+      '#jamstack101 #jamstackhandbook\n',
+      'https://jamstackhandbook.com',
+    ]
+  });
+
+  function handleOnTwitterClick(e) {
+    e.preventDefault();
+    openTweet({
+        message: twitterAction
+    })
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -29,6 +49,10 @@ export default function Home() {
             <p>
               Questions or issues? Reach out at hello@colbyfayock.com
             </p>
+
+            <h2>Let people know you just pre-ordered!</h2>
+
+            <Button onClick={handleOnTwitterClick}>Share on Twitter</Button>
 
           </Container>
         </Section>
