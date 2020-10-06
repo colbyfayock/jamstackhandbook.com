9bfc38e98a56c675f66001dc64a6a48fb50be556
colbyfayock <fayock@gmail.com>
2020-10-05 22:42:18 -0400

    testing coupon

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,44 @@
+import { Helmet } from 'react-helmet';
+import styles from './Layout.module.scss';
+
+import useSite from 'hooks/use-site';
+
+import Nav from 'components/Nav';
+import Main from 'components/Main';
+import Footer from 'components/Footer';
+import ShareAnchor from 'components/ShareAnchor';
+
+const Layout = ({ children, displayNav = true }) => {
+  const { metadata = {} } = useSite();
+  const { siteName, homepage } = metadata;
+
+  const pageTitle = siteName;
+
+  const helmetSettings = {
+    defaultTitle: pageTitle,
+    titleTemplate: `%s - ${pageTitle}`
+  }
+
+  return (
+    <div className={styles.layoutContainer}>
+      <Helmet {...helmetSettings}>
+        <link rel="icon" href="/favicon.ico" />
+        <meta property="og:title" content={pageTitle} />
+        <meta property="og:url" content={homepage} />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content={pageTitle} />
+      </Helmet>
+
+      { displayNav && <Nav />}
+
+      <Main>{ children }</Main>
+
+      <div>
+        <ShareAnchor />
+        <Footer />
+      </div>
+    </div>
+  )
+}
+
+export default Layout;
